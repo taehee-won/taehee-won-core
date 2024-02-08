d5b58960a83a8222847b2cb41e40a65a5981eb84
taehee-won <taehee.won@gmail.com>
2024-02-08 12:53:29 +0900

    feat: refactor interval parameter to value and optimize it for faster test

--- src/library/lib/Interval.py
+++ src/library/lib/Interval.py
@@ -1,4 +1,4 @@
-from typing import Final, Callable, Optional, Union
+from typing import Final, Optional, Union
 from time import time, sleep
 
 from .macro import ATTR, KWARGS_STR
@@ -11,21 +11,34 @@ _SECONDS_PER_DAY: Final[int] = 86400
 class Interval:
     def __init__(
         self,
-        interval: Union[int, float],
+        value: Union[int, float],
         name: Optional[str] = None,
     ):
-        self._interval: Union[int, float] = interval
+        self._value: Union[int, float] = value
         self._name: Union[str, None] = name
         self._trace: Trace = ATTR(Interval, "trace", lambda: Trace("core"))
         self._record = time() - _SECONDS_PER_DAY
 
+    def _moment(self) -> Union[int, float]:
+        return self._value - (time() - self._record)
+
     def __str__(self) -> str:
-        attrs = KWARGS_STR(name=self._name, interval=f"{self._interval}s")
+        attrs = KWARGS_STR(value=f"{self._value}s", name=self._name)
         return f"{self.__class__.__name__}({attrs})"
 
+    def print(self) -> None:
+        info = self._trace.info
+
+        if (moment := self._moment()) > 0:
+            info(f"{self}: interval {moment:.5f} seconds")
+
+        else:
+            info(f"{self}: no interval")
+
     def wait(self) -> Union[int, float]:
-        if (moment := self._interval - (time() - self._record)) > 0:
-            self._trace.debug(f"delay {moment:.5f} seconds by {self}")
+        moment = self._moment()
+        if moment > 0:
+            self._trace.debug(f"wait {moment:.5f} seconds by {self}")
             sleep(moment)
 
         self._record = time()
--- tests/library/lib/test_Interval.py
+++ tests/library/lib/test_Interval.py
@@ -5,32 +5,36 @@ from src.library.lib.Interval import Interval
 
 
 class TestInterval(TestCase):
-    def test_str(self):
+    def test_init(self):
         interval = Interval(1.00)
-        self.assertIsInstance(str(interval), str)
-        self.assertIn("Interval", str(interval))
-        self.assertNotIn("name:", str(interval))
-        self.assertIn("interval:", str(interval))
+        self.assertIsInstance(interval, Interval)
 
-        interval = Interval(1.00, name="")
+    def test_str(self):
+        interval = Interval(1)
         self.assertIsInstance(str(interval), str)
         self.assertIn("Interval", str(interval))
-        self.assertIn("name:", str(interval))
-        self.assertIn("interval:", str(interval))
+        self.assertIn("value:1", str(interval))
+        self.assertNotIn("name:", str(interval))
+
+        interval = Interval(1.00, name="TestInterval")
+        self.assertIsInstance(str(interval), str)
+        self.assertIn("Interval", str(interval))
+        self.assertIn("value:1", str(interval))
+        self.assertIn("name:TestInterval", str(interval))
 
     def test_wait(self):
-        interval = Interval(0.5)
+        interval = Interval(0.1)
         start = time()
         wait = interval.wait()
         self.assertTrue(0 < time() - start < 0.02)
         self.assertEqual(wait, 0)
 
         wait = interval.wait()
-        self.assertTrue(0.5 < time() - start < 0.52)
-        self.assertTrue(0.4 < wait < 0.5)
+        self.assertTrue(0.1 < time() - start < 0.12)
+        self.assertTrue(0.08 < wait < 0.1)
 
-        sleep(0.2)
+        sleep(0.05)
 
         wait = interval.wait()
-        self.assertTrue(1 < time() - start < 1.02)
-        self.assertTrue(0.2 < wait < 0.3)
+        self.assertTrue(0.2 < time() - start < 0.22)
+        self.assertTrue(0.03 < wait < 0.05)
